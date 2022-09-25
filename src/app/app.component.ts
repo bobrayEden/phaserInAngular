97411d1dbbedccebc90b26e760997917f8910966
bobrayEden <o.nocaudie@googlemail.com>
2022-09-25 21:08:44 +0200

    phaser tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'phaser-test';
+
+  newGameCount: BehaviorSubject<number> = new BehaviorSubject(0);
+
+  launchNewGame() {
+    this.newGameCount.next(1);
+  }
 }
